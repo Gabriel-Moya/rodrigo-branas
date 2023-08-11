d982baffeefafc4379be55760b5d45a89e316ecf
Gabriel Moya <gabrielmoya123@gmail.com>
2023-08-10 23:58:24 -0300

    REST API #4

--- playlist-yt-rest-api/server/route/postsRoute.js
+++ playlist-yt-rest-api/server/route/postsRoute.js
@@ -2,26 +2,42 @@ const express = require('express');
 const router = express.Router();
 const postService = require('../service/postsService');
 
-router.get('/posts', async function (req, res) {
-  const posts = await postService.getPosts();
-  res.json(posts);
+router.get('/posts', async function (req, res, next) {
+  try {
+    const posts = await postService.getPosts();
+    res.json(posts);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.post('/posts', async function (req, res) {
+router.post('/posts', async function (req, res, next) {
   const post = req.body;
-  const newPost = await postService.savePost(post);
-  res.json(newPost);
+  try {
+    const newPost = await postService.savePost(post);
+    res.status(201).json(newPost);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.put('/posts/:id', async function (req, res) {
+router.put('/posts/:id', async function (req, res, next) {
   const post = req.body;
-  await postService.updatePost(req.params.id, post);
-  res.end();
+  try {
+    await postService.updatePost(req.params.id, post);
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.delete('/posts/:id', async function (req, res) {
-  await postService.deletePost(req.params.id);
-  res.end();
+router.delete('/posts/:id', async function (req, res, next) {
+  try {
+    await postService.deletePost(req.params.id);
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;

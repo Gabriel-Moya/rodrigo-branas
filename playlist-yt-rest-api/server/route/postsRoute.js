80dc45f68bc17c0f7a664597b88fb05b89185d4f
Gabriel Moya <gabrielmoya123@gmail.com>
2023-08-10 21:09:50 -0300

    REST API #3

--- playlist-yt-rest-api/server/route/postsRoute.js
+++ playlist-yt-rest-api/server/route/postsRoute.js
@@ -7,20 +7,21 @@ router.get('/posts', async function (req, res) {
   res.json(posts);
 });
 
-router.get('/posts/:id', async function (req, res) {
-
-});
-
 router.post('/posts', async function (req, res) {
-
+  const post = req.body;
+  const newPost = await postService.savePost(post);
+  res.json(newPost);
 });
 
 router.put('/posts/:id', async function (req, res) {
-
+  const post = req.body;
+  await postService.updatePost(req.params.id, post);
+  res.end();
 });
 
 router.delete('/posts/:id', async function (req, res) {
-
+  await postService.deletePost(req.params.id);
+  res.end();
 });
 
 module.exports = router;
